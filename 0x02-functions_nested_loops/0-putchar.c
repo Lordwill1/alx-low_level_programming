9b6a8529deda70a317ed9957e846c05255bc2bd8
Lordwill1 <messagelordwill@gmail.com>
2022-09-13 07:05:38 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,13 @@
+#include <unistd.h>
+
 /**
- * @file 0-putchar.c
- * @author Godswill Kalu
- * @brief Algorithm to print a message in a specific language
- * @date 2022-09-13
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-#include "main.h"
-
-/**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }

ebc0f699efadc33e6be5e12804516b1b5b4b6ce2
Lordwill1 <messagelordwill@gmail.com>
2022-09-13 07:01:07 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,23 @@
  * @date 2022-09-13
  *
  */
-#include <main.h>
+#include "main.h"
 
 /**
- * main -  function is the entry point of the program
- *
- * Returns: 0 if the program ends successfully
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *message = "_putchar";
-	_putchar(message);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }

f97a28eb7349ddf92f043807d7318061b0cf30f5
Lordwill1 <messagelordwill@gmail.com>
2022-09-14 15:12:01 -0700

    Added task 0 and README.md

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,5 @@
 
 void positive_or_negative(int i);
 
+
 #endif /* MAIN_H */

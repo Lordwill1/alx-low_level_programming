f32b72f84be514a5ec71afa04c5f3df0a77cadf7
Lordwill1 <messagelordwill@gmail.com>
2022-09-14 15:00:08 -0700

    Added task 0 and README.md

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */

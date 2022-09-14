61c5846b9128060c6c226e309aba8c3b748e6c6e
Lordwill1 <messagelordwill@gmail.com>
2022-09-14 15:27:48 -0700

    Added task 1 on project 0x03 .C

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10) */
+	/*{ */
+	/*      putchar(i);*/
+	/*} */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}

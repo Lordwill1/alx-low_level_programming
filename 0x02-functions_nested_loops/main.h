7c09dd2d100809d0c3ada828ca5ddbd53b55ff60
Ottis Jennifer <105177827+Ojcee@users.noreply.github.com>
2022-09-15 23:38:38 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifendf _main_h_
+#define _main_h_
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif

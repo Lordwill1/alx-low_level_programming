af4c7e1e3eeccfefba1d6a645ecc19ab9c731de6
Lordwill1 <messagelordwill@gmail.com>
2022-09-14 17:47:50 -0700

    fixed bug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 i	{
-		if (month >= 2)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}

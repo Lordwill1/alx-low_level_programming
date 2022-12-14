5d5b72d58c5841f10ec2d58138ba1f2f53a5495b
Levon-coder1 <116287631+Levon-coder1@users.noreply.github.com>
2022-12-14 04:37:36 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,7 +21,6 @@ int main(void)
 		k = k + j;
 		j = k - j;
 	}
-
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
@@ -36,8 +35,6 @@ int main(void)
 		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
-
 	printf("\n");
-
 	return (0);
 }
